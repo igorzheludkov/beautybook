00cfbf028ded2a28d58b8b72b1a17a5e7cdcb509
Ihor Zheludkov <500griven@gmail.com>
2022-07-31 13:10:45 +0300

    123123

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,8 +11,8 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationUrl:
-        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+      // authorizationUrl:
+      //   'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
       // scope:
       //   ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
        
@@ -31,13 +31,17 @@ export default NextAuth({
   // secret: 'd339ab14c0829f1e4ecf94196f2a743d',
 
   callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
+    async jwt({ token }) {
+      token.userRole = "admin"
       return token
     },
+    // async jwt({ token, account }) {
+    //   // Persist the OAuth access_token to the token right after signin
+    //   if (account) {
+    //     token.accessToken = account.access_token
+    //   }
+    //   return token
+    // },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token from a provider.
       session.accessToken = token.accessToken
