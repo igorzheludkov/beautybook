49cf21ba562dcb947657707bf323c7277b10fcc3
Ihor Zheludkov <500griven@gmail.com>
2022-07-31 12:05:11 +0300

    update env config

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,8 +18,7 @@ export default NextAuth({
     }),
   ],
   jwt: {
-    // The maximum age of the NextAuth.js issued JWT in seconds.
-    // Defaults to `session.maxAge`.
+
     maxAge: 60 * 60 * 24 * 30,
     // You can define your own encode/decode functions for signing and encryption
     async encode() {},
