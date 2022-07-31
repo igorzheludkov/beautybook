fbde75910942f6a0ab1b84a68fd0b15ac800b8ed
Ihor Zheludkov <500griven@gmail.com>
2022-07-31 11:50:10 +0300

    jwt config 11.50

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -17,17 +17,17 @@ export default NextAuth({
         'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly',
     }),
   ],
-  // jwt: {
-  //   // The maximum age of the NextAuth.js issued JWT in seconds.
-  //   // Defaults to `session.maxAge`.
-  //   maxAge: 60 * 60 * 24 * 30,
-  //   // You can define your own encode/decode functions for signing and encryption
-  //   async encode() {},
-  //   async decode() {},
-  // }
   jwt: {
-    encryption: true,
+    // The maximum age of the NextAuth.js issued JWT in seconds.
+    // Defaults to `session.maxAge`.
+    maxAge: 60 * 60 * 24 * 30,
+    // You can define your own encode/decode functions for signing and encryption
+    async encode() {},
+    async decode() {},
   },
+  // jwt: {
+  //   encryption: true,
+  // },
   // secret: 'd339ab14c0829f1e4ecf94196f2a743d',
 
   callbacks: {
