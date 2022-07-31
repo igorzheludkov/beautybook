19b318816e10df1158dba3ae02c6046b5613215b
Ihor Zheludkov <500griven@gmail.com>
2022-07-31 13:08:25 +0300

    65465

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,23 +11,20 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      // authorizationUrl:
-      //   'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+      authorizationUrl:
+        'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
       // scope:
       //   ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'],
-        authorization: {
-          url: "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
-          params: { scope: "email" }
-        }
+       
     }),
   ],
-  jwt: {
+  // jwt: {
 
-    maxAge: 60 * 60 * 24 * 30,
-    // You can define your own encode/decode functions for signing and encryption
-    async encode() {},
-    async decode() {},
-  },
+  //   maxAge: 60 * 60 * 24 * 30,
+  //   // You can define your own encode/decode functions for signing and encryption
+  //   async encode() {},
+  //   async decode() {},
+  // },
   // jwt: {
   //   encryption: true,
   // },
