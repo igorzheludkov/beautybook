fdf1c939e564819869eea3af333c96c7545882cd
Ihor Zheludkov <500griven@gmail.com>
2022-07-31 11:44:58 +0300

    23423423

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -18,6 +18,14 @@ export default NextAuth({
     }),
   ],
   // jwt: {
+  //   // The maximum age of the NextAuth.js issued JWT in seconds.
+  //   // Defaults to `session.maxAge`.
+  //   maxAge: 60 * 60 * 24 * 30,
+  //   // You can define your own encode/decode functions for signing and encryption
+  //   async encode() {},
+  //   async decode() {},
+  // }
+  // jwt: {
   //   encryption: true,
   // },
   // secret: 'd339ab14c0829f1e4ecf94196f2a743d',
