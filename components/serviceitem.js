0f17059a72dceea476dd042b2392957289610a8d
Ihor Zheludkov <500griven@gmail.com>
2022-08-13 23:17:09 +0300

    minor styling

--- components/serviceitem.js
+++ components/serviceitem.js
@@ -41,92 +41,100 @@ export default function ServiceItem({ data, user }) {
   const [fullView, setFullView] = useState('0')
 
   function fullViewHandler(e) {
-    switch(e.target.value) {
-        case '1': {
-            setFullView('1')
-        } break
-        case '0': {
-            setFullView('0')
-        } break
+    switch (e.target.value) {
+      case '1':
+        {
+          setFullView('1')
+        }
+        break
+      case '0':
+        {
+          setFullView('0')
+        }
+        break
     }
   }
 
-  const view = fullView === '1' ? {width: '300px', height: '300px'} : {}
+  const view = fullView === '1' ? { width: '300px', height: '300px' } : {}
 
   return (
-    <div className={s.serv_inner}>
-      <div className={s.serv_item}>
-        <span className={s.serv_title}>{data.item_1.name}</span>
-        <span className={s.serv_price}>{data.item_1.price} грн</span>
-        <span className={s.serv_duration}>{data.item_1.dur} хв</span>
-      </div>
-      <div className={s.serv_item}>
-        {data.item_2.name && (
-          <>
-            <span className={s.serv_subtitle}>{data.item_2.name}</span>
-            <span className={s.serv_subprice}>{data.item_2.price} грн</span>
-            <span className={s.serv_subduration}>{data.item_2.dur} хв</span>
-          </>
-        )}
-      </div>
-      <div className={s.serv_item}>
-        {data.item_3.name && (
-          <>
-            <span className={s.serv_subtitle}>{data.item_3.name}</span>
-            <span className={s.serv_subprice}>{data.item_3.price} грн</span>
-            <span className={s.serv_subduration}>{data.item_3.dur} хв</span>
-          </>
-        )}
-      </div>
-      {/* <div className={s.serv_desc}>{data.about.description}</div> */}
-      <div className={s.images}>
-        <div className={s.images_wrapper}>
-          {data.pic.map((i) => (
-            <div key={i} className={s.images_container} style={view}>
-              <Image
-                style={{ borderRadius: '10px' }}
-                layout='responsive'
-                objectFit='cover'
-                width={80}
-                height={80}
-                key={i}
-                src={i}
-                alt='service'
-              />
+    <>
+      <div className={s.serv_wrapper}>
+        <div className='container'>
+          <div className={s.serv_inner}>
+            <div className={s.serv_item}>
+              <span className={s.serv_title}>{data.item_1.name}</span>
+              <span className={s.serv_price}>{data.item_1.price} грн</span>
+              <span className={s.serv_duration}>{data.item_1.dur} хв</span>
             </div>
-          ))}
+            <div className={s.serv_item}>
+              {data.item_2.name && (
+                <>
+                  <span className={s.serv_subtitle}>{data.item_2.name}</span>
+                  <span className={s.serv_subprice}>{data.item_2.price} грн</span>
+                  <span className={s.serv_subduration}>{data.item_2.dur} хв</span>
+                </>
+              )}
+            </div>
+            <div className={s.serv_item}>
+              {data.item_3.name && (
+                <>
+                  <span className={s.serv_subtitle}>{data.item_3.name}</span>
+                  <span className={s.serv_subprice}>{data.item_3.price} грн</span>
+                  <span className={s.serv_subduration}>{data.item_3.dur} хв</span>
+                </>
+              )}
+            </div>
+            <div className={s.serv_desc}>{data.about.description}</div>
+          </div>
         </div>
-      </div>
 
-      {fullView === '0' ? (
-        <button
-          className={s.gallery}
-          onClick={fullViewHandler}
-          value='1'
-        >
-          Дивитись фото 
-        </button>
-      ) : (
-        <button className={s.gallery} onClick={fullViewHandler} value='0'>
-          Згорнути фото
-        </button>
-      )}
-      {checked ? (
-        <button
-          className={s.gotocart}
-          onClick={() => {
-            router.push({
-              pathname: '/orders',
-            })
-          }}
-        >
-          Вибрати час і забронювати
-        </button>
-      ) : (
-        <button className={s.addtocart} onClick={cartHandler}>
-          Додати в кошик
-        </button>
-      )}
-    </div>
+        <div className={s.images}>
+          <div className={s.images_wrapper}>
+            {data.pic.map((i) => (
+              <div key={i} className={s.images_container} style={view}>
+                <Image
+                  style={{ borderRadius: '10px' }}
+                  layout='responsive'
+                  objectFit='cover'
+                  width={80}
+                  height={80}
+                  key={i}
+                  src={i}
+                  alt='service'
+                />
+              </div>
+            ))}
+          </div>
+          {fullView === '0' ? (
+          <button className={s.gallery} onClick={fullViewHandler} value='1'>
+            Дивитись фото
+          </button>
+        ) : (
+          <button className={s.gallery} onClick={fullViewHandler} value='0'>
+            Згорнути фото
+          </button>
+        )}
+        {checked ? (
+          <button
+            className={s.gotocart}
+            onClick={() => {
+              router.push({
+                pathname: '/orders',
+              })
+            }}
+          >
+            Вибрати час і забронювати
+          </button>
+        ) : (
+          <button className={s.addtocart} onClick={cartHandler}>
+            Додати в кошик
+          </button>
+        )}
+        </div>
+
+        
+      </div>
+    </>
   )
 }
--- pages/catalog/[id].js
+++ pages/catalog/[id].js
@@ -23,93 +23,98 @@ export default function ServiceEdit() {
   const { data: services } = useSWR(`/api/services/${router.query.id}`, fetcher)
 
   return user && services ? (
-    <div className='container'>
-      <div className={s.header}>
-        <div className={s.avatar_container}>
-          <div className={s.avatar_img_inner}>
-            <Image
-              className={s.avatar_img}
-              placeholder='blur'
-              blurDataURL='/images/userplaceholder.png'
-              layout='responsive'
-              objectFit='cover'
-              width={150}
-              height={150}
-              src={user.userData.photo ? user.userData.photo : '/images/userplaceholder.png'}
-              alt='avatar'
-            />
-            <div className={s.bookmark_add}>
-              <Image width={20} height={20} src='/images/bookmarks.png' alt='instagram' />
+    <>
+      <div className='container'>
+        <div className={s.header}>
+          <div className={s.avatar_container}>
+            <div className={s.avatar_img_inner}>
+              <Image
+                className={s.avatar_img}
+                placeholder='blur'
+                blurDataURL='/images/userplaceholder.png'
+                layout='responsive'
+                objectFit='cover'
+                width={150}
+                height={150}
+                src={user.userData.photo ? user.userData.photo : '/images/userplaceholder.png'}
+                alt='avatar'
+              />
+              <div className={s.bookmark_add}>
+                <Image width={20} height={20} src='/images/bookmarks.png' alt='instagram' />
+              </div>
+            </div>
+
+            <div className={s.social}>
+              <Link href={user.userData.social_1}>
+                <a target='_blank' className='social_link'>
+                  <Image width={30} height={30} src='/images/instagram.png' alt='instagram' />
+                </a>
+              </Link>
+              <Link href={user.userData.social_2}>
+                <a target='_blank' className='social_link'>
+                  <Image width={30} height={30} src='/images/telegram.png' alt='instagram' />
+                </a>
+              </Link>
+              <Link href={`tel:${user.userData.phone}`}>
+                <a className='social_link'>
+                  <Image width={30} height={30} src='/images/phone.png' alt='instagram' />
+                </a>
+              </Link>
             </div>
           </div>
-
-          <div className={s.social}>
-            <Link href={user.userData.social_1}>
-              <a target='_blank' className='social_link'>
-                <Image width={30} height={30} src='/images/instagram.png' alt='instagram' />
-              </a>
-            </Link>
-            <Link href={user.userData.social_2}>
-              <a target='_blank' className='social_link'>
-                <Image width={30} height={30} src='/images/telegram.png' alt='instagram' />
-              </a>
-            </Link>
-            <Link href={`tel:${user.userData.phone}`}>
-              <a className='social_link'>
-                <Image width={30} height={30} src='/images/phone.png' alt='instagram' />
-              </a>
-            </Link>
+          <div className={s.header_block}>
+            <div className={s.header_name}>
+              <div className={s.title}>{user.userData.name}</div>
+              <div className={s.title}>{user.userData.surname}</div>
+              <div className={s.spec_wrapper}>
+                {user.userData.categories.map((i) => (
+                  <div key={i} className={s.spec}>
+                    {i}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={s.about_me}>{user.userData.about_me}</div>
           </div>
         </div>
-        <div className={s.header_block}>
-          <div className={s.header_name}>
-            <div className={s.title}>{user.userData.name}</div>
-            <div className={s.title}>{user.userData.surname}</div>
-            <div className={s.spec_wrapper}>
-              {user.userData.categories.map((i) => (
-                <div key={i} className={s.spec}>
-                  {i}
-                </div>
-              ))}
+
+        <div className={s.adress_info}>
+          <div className={s.info_a}>
+            <div className={s.info_a_logo}>
+              <Image width={20} height={20} src='/images/adress.png' alt='adress' />
+            </div>
+            <div className={s.info_a_text}>
+              {user.userData.city}
+              <br></br>
+              {user.userData.street}
+              <br></br>
+              {user.userData.location}
             </div>
           </div>
-          <div className={s.about_me}>{user.userData.about_me}</div>
+          <div className={s.info_h}>
+            <div className={s.info_h_logo}>
+              <Image width={20} height={20} src='/images/orders.png' alt='instagram' />
+            </div>
+            <div className={s.daytime}>
+              <div className={s.working_days}>{user.userData.work_days}</div>
+              <div className={s.working_time}>
+                {user.userData.work_begin}-{user.userData.work_end}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className={s.adress_info}>
-        <div className={s.info_a}>
-          <div className={s.info_a_logo}>
-            <Image width={20} height={20} src='/images/adress.png' alt='adress' />
-          </div>
-          <div className={s.info_a_text}>
-            {user.userData.city}
-            <br></br>
-            {user.userData.street}
-            <br></br>
-            {user.userData.location}
-          </div>
-        </div>
-        <div className={s.info_h}>
-          <div className={s.info_h_logo}>
-            <Image width={20} height={20} src='/images/orders.png' alt='instagram' />
-          </div>
-          <div className={s.daytime}>
-            <div className={s.working_days}>{user.userData.work_days}</div>
-            <div className={s.working_time}>
-              {user.userData.work_begin}-{user.userData.work_end}
-            </div>
-          </div>
-        </div>
-      </div>
       <section className={s.services}>
-        <h1 className={s.title_h2}>Послуги</h1>
+        <div className='container'>
+          <h1 className={s.title_h2}>Послуги</h1>
+        </div>
         <div className='serv_wrapper'>
           {services.services.map((i) => (
             <ServiceItem key={i._id} data={i} user={user} />
           ))}
         </div>
       </section>
-    </div>
+    </>
   ) : null
 }
