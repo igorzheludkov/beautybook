a1a0fbfdc86f9c1189c621a4ce23b7f85bcc0d8a
Ihor Zheludkov <500griven@gmail.com>
2022-08-14 11:05:48 +0300

    styling profile page

--- pages/landing.js
+++ pages/landing.js
@@ -1,21 +1,28 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
 import s from '../styles/landing.module.css'
-import {signIn } from 'next-auth/react'
-
+import { signIn } from 'next-auth/react'
 
 export default function Login() {
-
   return (
     <>
       <Head>
         <title>Каталог краси</title>
       </Head>
-      <h1>Реєстрація та вхід</h1>
-      <p>На даний момент доступна реєстрація через обліковий запис Google. З часом додадуться інші варіанти</p>
-      <h2>Для входу натисніть кнопку Google</h2>
-      
-      <button className={s.button}  onClick={()=> signIn('google')} >Зареєструватись та увійти</button>
+      <div className='container'>
+        <h1>Каталог Краси</h1>
+        <Image src='/images/site.png' width={100} height={100} alt='personal site' />
+        <h2 className={s.title_h2}>Персональний міні-сайт</h2>
+        <Image src='/images/booking.png' width={100} height={100} alt='personal site' />
+        <h2 className={s.title_h2}>Інтуїтивна система онлайн-бронювання</h2>
+        <Image src='/images/profile.png' width={100} height={100} alt='personal site' />
+        <h2 className={s.title_h2}>Публікація в каталозі спеціалістів</h2>
+      <button className={s.button} onClick={() => signIn('google')}>
+        Зареєструватись та увійти
+      </button>
+      </div>
     </>
+
   )
 }
--- pages/user/profile.js
+++ pages/user/profile.js
@@ -12,211 +12,290 @@ import ScrollBox from '../../components/scrollbox'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
-
 export default function PersonalPage({ user, data }) {
   const router = useRouter()
-console.log(router);
-    console.log(user.email)
-    const { data: session, status } = useSession()
-    const [userPublic, setUserPublic] = useState('0')
-    const [form, setForm] = useState({
-        photo: '',
-        about_me: '',
-        city: '',
-        email: '',
-        location: '',
-        name: '',
-        phone: '',
-        social_1: '',
-        social_2: '',
-        categories: [],
-        street: '',
-        surname: '',
-        work_begin: '',
-        work_end: '',
-        interval: 10,
-        horizon: 3,
-        url: '',
-        work_days: '',
+  console.log(router)
+  console.log(user.email)
+  const { data: session, status } = useSession()
+  const [userPublic, setUserPublic] = useState('0')
+  const [form, setForm] = useState({
+    photo: '',
+    about_me: '',
+    city: '',
+    email: '',
+    location: '',
+    name: '',
+    phone: '',
+    social_1: '',
+    social_2: '',
+    categories: [],
+    street: '',
+    surname: '',
+    work_begin: '',
+    work_end: '',
+    interval: 10,
+    horizon: 3,
+    url: '',
+    work_days: '',
+  })
+  const [avatar, setAvatar] = useState('')
+
+  const categories = data.categories
+
+  useEffect(() => {
+    if (session?.user.email) {
+      fetch(`/api/userdata?q=${session.user.email}`, {
+        method: 'GET',
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data === null) {
+            console.log('new user')
+            newUser()
+          } else {
+            setUserPublic(data)
+            setForm(data.userData)
+            console.log('registered user')
+          }
+        })
+    }
+  }, [session])
+
+  async function newUser() {
+    const response = await fetch('/api/userdata', {
+      method: 'PUT',
+      body: JSON.stringify({ email: session.user.email, userData: form }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
-    const [avatar, setAvatar] = useState('')
+    const data = await response.json()
+  }
 
+  function inputHandler(e) {
+    let key = e.target.id
+    setForm({ ...form, [key]: e.target.value })
+  }
 
-    const categories = data.categories
-
-    useEffect(() => {
-        if (session?.user.email) {
-            fetch(`/api/userdata?q=${session.user.email}`, {
-                method: 'GET',
-            })
-                .then((res) => res.json())
-                .then((data) => {
-                    if (data === null) {
-                        console.log('new user')
-                        newUser()
-                    } else {
-                        setUserPublic(data)
-                        setForm(data.userData)
-                        console.log('registered user')
-                    }
-                })
-        }
-    }, [session])
-
-    async function newUser() {
-        const response = await fetch('/api/userdata', {
-            method: 'PUT',
-            body: JSON.stringify({ email: session.user.email, userData: form }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        const data = await response.json()
+  function checkboxToggle(e) {
+    if (form.categories) {
+      e.target.checked
+        ? setForm({ ...form, categories: [...form.categories, e.target.value] })
+        : setForm({ ...form, categories: form.categories.filter((i) => i !== e.target.value) })
+    } else {
+      setForm({ ...form, categories: [e.target.value] })
     }
+  }
 
-    function inputHandler(e) {
-        let key = e.target.id
-        setForm({ ...form, [key]: e.target.value })
-    }
+  async function buttonHandler(e) {
+    e.preventDefault(e)
+    const response = await fetch('/api/userdata', {
+      method: 'POST',
+      body: JSON.stringify({ email: session.user.email, userData: form }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const data = await response.json()
+    console.log('Sended')
+  }
 
-    function checkboxToggle(e) {
-        if (form.categories) {
-            e.target.checked
-                ? setForm({ ...form, categories: [...form.categories, e.target.value] })
-                : setForm({ ...form, categories: form.categories.filter((i) => i !== e.target.value) })
-        } else {
-            setForm({ ...form, categories: [e.target.value] })
-        }
-    }
+  function avatarHandler(result) {
+    setAvatar(result)
+    setForm(() => ({ ...form, photo: result.secure_url }))
+  }
+  const profileNamePhone = [
+    { id: 'name', tp: 'text', vl: 'Ваше ім`я' },
+    { id: 'surname', tp: 'text', vl: 'Ваше прізвище' },
+    { id: 'phone', tp: 'text', vl: 'Ваш номер телефону' },
+  ]
+  const social = [
+    { id: 'social_1', tp: 'text', vl: 'Ваш нікнейм в instagram', icon: '/images/instagram.png' },
+    { id: 'social_2', tp: 'text', vl: 'Ваш нікнейм в telegram', icon: '/images/telegram.png' },
+  ]
+  const aboutMe = {
+    id: 'about_me',
+    tp: 'text',
+    vl: 'Розкажіть коротко про себе. Це речення буде на головній сторінці біля фото',
+    icon: '/images/profile.png',
+  }
+  const workplace = [
+    { id: 'city', tp: 'text', vl: 'Ваше місто', icon: '/images/adress.png' },
+    { id: 'street', tp: 'text', vl: 'Вулиця', icon: '/images/adress.png' },
+    { id: 'location', tp: 'text', vl: 'Будівля або салон для орієнтиру', icon: '/images/adress.png' },
+  ]
+  const personalInfo = [
+    { id: 'work_begin', tp: 'text', vl: 'О котрій годині починаєте працювати?' },
+    { id: 'work_end', tp: 'text', vl: 'Коли закінчуєте робочий день?' },
+    { id: 'interval', tp: 'text', vl: 'Скільки хвилин має бути перерва між прийомом клієнтів?' },
+    { id: 'horizon', tp: 'text', vl: 'На скільки місяців наперед можуть записуватись люди?' },
+    { id: 'work_days', tp: 'text', vl: 'Вкажіть робочі дні' },
+  ]
 
-    async function buttonHandler(e) {
-        e.preventDefault(e)
-        const response = await fetch('/api/userdata', {
-            method: 'POST',
-            body: JSON.stringify({ email: session.user.email, userData: form }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        const data = await response.json()
-        console.log('Sended')
-    }
+  if (!form) return <div>Loading...</div>
 
-    function avatarHandler(result) {
-        setAvatar(result)
-        setForm(() => ({ ...form, photo: result.secure_url }))
-    }
+  return (
+    <>
+      <Head>
+        <title>Налаштування персональних даних</title>
+      </Head>
+      <Script src='https://upload-widget.cloudinary.com/global/all.js' strategy='afterInteractive' />
 
-    const personalInfo = [
-        { id: 'name', tp: 'text', vl: 'Ваше ім`я' },
-        { id: 'surname', tp: 'text', vl: 'Ваше прізвище' },
-        { id: 'phone', tp: 'text', vl: 'Ваше номер телефону' },
-        { id: 'social_1', tp: 'text', vl: 'Ваш акаунт в instagram' },
-        { id: 'social_2', tp: 'text', vl: 'Ваш акаунт в telegram' },
-        { id: 'about_me', tp: 'text', vl: 'Розкажіть трохи про себе' },
-        { id: 'city', tp: 'text', vl: 'Ваше місто' },
-        { id: 'street', tp: 'text', vl: 'Вулиця' },
-        { id: 'location', tp: 'text', vl: 'Будівля або салон для орієнтиру' },
-        { id: 'work_begin', tp: 'text', vl: 'О котрій годині починаєте працювати?' },
-        { id: 'work_end', tp: 'text', vl: 'Коли закінчуєте робочий день?' },
-        { id: 'interval', tp: 'text', vl: 'Скільки хвилин має бути перерва між прийомом клієнтів?' },
-        { id: 'horizon', tp: 'text', vl: 'На скільки місяців наперед можуть записуватись люди?' },
-        { id: 'work_days', tp: 'text', vl: 'Вкажіть робочі дні' },
-    ]
+      <MasterNav path='/' status='active_tab' />
 
-    if (!form) return <div>Loading...</div>
+      <div className='container'>
+        <div className={s.profile_nav}>
+          <button className={s.nav_button}>
+            <Link href={`/catalog/${user.email}`}>
+              <a>Відкрити вашу сторінку</a>
+            </Link>
+          </button>
+          <button className={s.nav_button}>
+            <Link href='/api/auth/signout'>
+              <a
+                onClick={(e) => {
+                  e.preventDefault()
+                  signOut()
+                }}
+              >
+                Вийти з системи
+              </a>
+            </Link>
+          </button>
+        </div>
 
-    return (
-        <>
-            <Head>
-                <title>Налаштування персональних даних</title>
-            </Head>
-            <Script src='https://upload-widget.cloudinary.com/global/all.js' strategy='afterInteractive' />
+        <div className={s.header}>
+          <div className={s.avatar_container}>
+            <Image
+              className={s.avatar_img}
+              layout='responsive'
+              objectFit='cover'
+              width={150}
+              height={150}
+              src={form.photo ? form.photo : '/images/userplaceholder.png'}
+              alt='avatar'
+            />
+            <Cloudinary uploadHandler={avatarHandler} multiple={false} />
+          </div>
 
-            <MasterNav path='/' status='active_tab' />
-
-            <div className='container'>
-                <div className={s.profile_nav}>
-                    <button className={s.nav_button}>
-                        <Link href={`/catalog/${user.email}`}>
-                            <a>Відкрити вашу сторінку</a>
-                        </Link>
-                    </button>
-                    <button className={s.nav_button}>
-                        <Link href='/api/auth/signout'>
-                            <a
-                                onClick={(e) => {
-                                    e.preventDefault()
-                                    signOut()
-                                }}
-                            >
-                                Вийти з системи
-                            </a>
-                        </Link>
-                    </button>
-                </div>
-
-                <div className={s.avatar_container}>
-                    <Image
-                        className={s.avatar_img}
-                        layout='responsive'
-                        objectFit='cover'
-                        width={150}
-                        height={150}
-                        src={form.photo ? form.photo : '/images/userplaceholder.png'}
-                        alt='avatar'
-                    />
-                    <Cloudinary uploadHandler={avatarHandler} multiple={false} />
-                </div>
-
-                {personalInfo?.map((i) => (
-                    <span className={s.input_item} key={i.id}>
-                        <label className={s.label} htmlFor={i.id}>
-                            {i.vl}
-                        </label>
-                        <input
-                            className={s.input_text}
-                            id={i.id}
-                            value={form[i.id]}
-                            onChange={inputHandler}
-                            type={i.tp}
-                        />
-                    </span>
-                ))}
-                <p>Виберіть до трьох спеціальностей</p>
+          <div className={s.name_wrapper}>
+            {profileNamePhone?.map((i) => (
+              <div className={s.name_container} key={i.id}>
+                <span className={s.input_item}>
+                  <input
+                    className={s.input_text}
+                    id={i.id}
+                    value={form[i.id]}
+                    onChange={inputHandler}
+                    type={i.tp}
+                    placeholder={i.vl}
+                  />
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={s.social_wrapper}>
+          {social?.map((i) => (
+            <div className={s.social_container} key={i.id}>
+              <Image src={i.icon} width={20} height={20} alt='social profile' />
+              <span className={s.input_item}>
+                <input
+                  className={s.input_text}
+                  id={i.id}
+                  value={form[i.id]}
+                  onChange={inputHandler}
+                  type={i.tp}
+                  placeholder={i.vl}
+                />
+              </span>
             </div>
-            <div>
-                <ScrollBox data={categories} checkboxToggle={checkboxToggle} checkStatus={form.categories} />
-            </div>
+          ))}
+        </div>
+        <div className={s.scrollbox_wrapper}>
+          <p className={s.paragraph}>Виділіть ваші навики</p>
+          <ScrollBox data={categories} checkboxToggle={checkboxToggle} checkStatus={form.categories} />
+        </div>
+        <div className={s.aboutme_wrapper}>
+          <div className={s.aboutme_container}>
+            <Image src={aboutMe.icon} width={20} height={20} alt='social profile' />
+            <span className={s.input_item}>
+              <textarea
+                placeholder={aboutMe.vl}
+                className={s.aboutme_input}
+                id={aboutMe.id}
+                value={form[aboutMe.id]}
+                onChange={inputHandler}
+                type={aboutMe.tp}
+              />
+            </span>
+          </div>
+        </div>
 
-            <div style={{ height: '150px', width: '100%', display: 'flex' }}>
-                <button
-                    style={{ width: '100px', margin: 'auto' }}
-                    onClick={buttonHandler}
-                    className={s.submit_btn}
-                >
-                    Зберегти
-                </button>
+        <h4>Додайте робочу адресу</h4>
+        <div className={s.workplace_wrapper}>
+          {workplace?.map((i) => (
+            <div className={s.social_container} key={i.id}>
+              <Image src={i.icon} width={20} height={20} alt='social profile' />
+              <span className={s.input_item}>
+                <input
+                  className={s.input_text}
+                  id={i.id}
+                  value={form[i.id]}
+                  onChange={inputHandler}
+                  type={i.tp}
+                  placeholder={i.vl}
+                />
+              </span>
             </div>
-        </>
-    )
+          ))}
+        </div>
+        <h4>Додайте ваш графік роботи</h4>
+        
+        <div className={s.worktime_wrapper}>
+          {personalInfo?.map((i) => (
+            <div className={s.worktime_container} key={i.id}>
+              <span className={s.input_item}>
+                <label className={s.label} htmlFor={i.id}>
+                  {i.vl}
+                </label>
+                <input
+                  className={s.input_text}
+                  id={i.id}
+                  value={form[i.id]}
+                  onChange={inputHandler}
+                  type={i.tp}
+                />
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div style={{ height: '150px', width: '100%', display: 'flex' }}>
+        <button style={{ width: '100px', margin: 'auto' }} onClick={buttonHandler} className={s.submit_btn}>
+          Зберегти
+        </button>
+      </div>
+    </>
+  )
 }
 
 export async function getServerSideProps(context) {
-    const session = await getSession(context)
-    if (!session) {
-        context.res.writeHead(302, { Location: '/login' })
-        context.res.end()
-        return {}
-    }
-    const res = await fetch(`${server}/api/categories`, {
-        method: 'GET',
-    })
-    const cat = await res.json()
-    return {
-        props: {
-            user: session.user,
-            data: cat,
-        },
-    }
+  const session = await getSession(context)
+  if (!session) {
+    context.res.writeHead(302, { Location: '/login' })
+    context.res.end()
+    return {}
+  }
+  const res = await fetch(`${server}/api/categories`, {
+    method: 'GET',
+  })
+  const cat = await res.json()
+  return {
+    props: {
+      user: session.user,
+      data: cat,
+    },
+  }
 }
