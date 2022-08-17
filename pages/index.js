777795e25a1ed5cab8a13cc34fd3898a9c72217a
Ihor Zheludkov <500griven@gmail.com>
2022-08-17 18:12:26 +0300

    added photos

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,18 @@ export default function Landing() {
         <title>Каталог краси</title>
       </Head>
       <div className='container'>
-      
         <Image src='/images/site.png' width={80} height={80} alt='personal site' />
         <h2 className={s.title_h2}>Персональний міні-сайт</h2>
         <Image src='/images/booking.png' width={80} height={80} alt='personal site' />
         <h2 className={s.title_h2}>Інтуїтивна система онлайн-бронювання</h2>
         <Image src='/images/profile.png' width={80} height={80} alt='personal site' />
         <h2 className={s.title_h2}>Публікація в каталозі спеціалістів</h2>
+        <h3>Подивитись як виглядає заповнений профіль можна <Link href='https://beautybook.vercel.app/62e6840acf4d88a22c64aeed'>
+          <a style={{color: 'blue'}}>тут</a>
+        </Link></h3>
+       
+<br></br>
+
         <button
           className={s.button}
           onClick={() =>
