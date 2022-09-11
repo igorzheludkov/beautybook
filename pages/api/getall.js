8b48c8db78b6d357d7881c7e7d73475e2104245b
Ihor Zheludkov <500griven@gmail.com>
2022-09-11 21:27:40 +0300

    fixed filter in api getall

--- pages/api/getall.js
+++ pages/api/getall.js
@@ -15,7 +15,13 @@ export default async function GetData(req, res) {
   const collection = db.collection('user_public')
   console.log('Searching for user...')
   const findUser = await collection
-    .find({ $and: [{ 'userData.isPageVisibleInCat': '1' }, { 'userData.city': req.body.city }, { 'userData.categories': [req.body.category] }] })
+    .find({
+      $and: [
+        { 'userData.isPageVisibleInCat': '1' },
+        { 'userData.city': req.body.city },
+        { 'userData.categories': req.body.category },
+      ],
+    })
     .toArray()
   await client.close()
   // }
